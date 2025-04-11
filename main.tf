3f07b57c997061dfbfa7bf04adb6468991ff24ed
aksh31at <138380099+aksh31at@users.noreply.github.com>
2025-04-12 01:55:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,24 @@ resource "aws_instance" "example" {
   ami           = "ami-0274f4b62b6ae3bd5"  # Amazon Linux 2
   instance_type = "t3.micro"
 }
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
