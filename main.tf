5f8e9bd973cf534e480274a5cf822242bfe7ec5f
aksh31at <138380099+aksh31at@users.noreply.github.com>
2025-04-12 21:51:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0274f4b62b6ae3bd5"  # Amazon Linux 2
   instance_type = "t3.micro"
-
   tags = {
     Name = "MyExampleInstance"
   }
@@ -17,14 +16,14 @@ resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
-        }
-        Action    = "sts:AssumeRole"
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
@@ -36,12 +35,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Lambda Function (code uploaded separately as lambda.zip)
+# Lambda Function (upload zip separately to repo or S3)
 resource "aws_lambda_function" "my_lambda" {
-  function_name    = "my_lambda_function"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
   filename         = "akshat-lambda-739ab9b7-191b-4013-834e-f077930b3ba2.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("akshat-lambda-739ab9b7-191b-4013-834e-f077930b3ba2.zip")
 }
