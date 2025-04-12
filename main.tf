b57fba2f6474df6783c0ff87d3fb59cb729eca86
aksh31at <138380099+aksh31at@users.noreply.github.com>
2025-04-12 21:08:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,21 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
-
-# IAM Role for Lambda
-data "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_execution_role_v3"  # ✅ change the name
-
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
+
 
 # Attach basic logging policy to the role
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
@@ -36,10 +32,9 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 # Lambda Function (upload zip separately to repo or S3)
 resource "aws_lambda_function" "my_lambda" {
   function_name = "my_lambda_function"
-  role          = data.aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_exec_role.name
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
-
   filename         = "akshat-lambda-739ab9b7-191b-4013-834e-f077930b3ba2.zip"
   source_code_hash = filebase64sha256("akshat-lambda-739ab9b7-191b-4013-834e-f077930b3ba2.zip")
 }
